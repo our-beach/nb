4ca11674ca9b422bc603f5467c2c8f007ca1332c
ericheep <ericheep@gmail.com>
2018-03-24 15:18:09 -0700

    :white_check_mark: (confirmation-code-service)
    Add tests for #create! #valid? #generate_code
    Add #generate_code

--- app/services/confirmation_code_service.rb
+++ app/services/confirmation_code_service.rb
@@ -2,9 +2,13 @@ class ConfirmationCodeService
   TTL = Figaro.env.confirmation_code_ttl.to_i.minutes
 
   def self.create!
-    code = Array.new(6) { rand(10) }.join
+    code = generate_code
     RedisDB.insert! code, true, ttl: TTL
     code
+  end
+
+  def self.generate_code
+    Array.new(6) { rand(10) }.join
   end
 
   def self.valid? code
--- spec/unit/services/confirmation_code_service_spec.rb
+++ spec/unit/services/confirmation_code_service_spec.rb
@@ -3,27 +3,39 @@
 require 'rails_helper'
 
 RSpec.describe ConfirmationCodeService do
-  describe 'create!' do
-    subject { described_class.create! }
+  describe 'generate_code' do
+    subject { described_class.generate_code }
 
     it { is_expected.to match(/^\d{6}$/) }
   end
 
+  describe 'create!' do
+    subject { described_class.create! }
+    let(:code) { '123456' }
+
+    before do
+      expect(RedisDB).to receive(:insert!).
+        with code, true, ttl: described_class::TTL
+
+      expect(described_class).to receive(:generate_code).
+        and_return code
+    end
+
+    it { is_expected.to eql code }
+  end
+
   describe 'valid?' do
     subject { described_class.valid? code }
+    let(:code) { "123456" }
 
-    context 'when the confirmation code exists in redis' do
-      let(:code) { '654321' }
-
-      before do
-        RedisDB.insert!(code, true, ttl: 1)
-      end
-
-      after do
-        RedisDB.delete(code)
-      end
-
+    context 'when the confirmation code has been generated' do
+      before { expect(RedisDB).to receive(:exists?).and_return true }
       it { is_expected.to be true }
+    end
+
+    context 'when the confirmation code has not been generated' do
+      before { expect(RedisDB).to receive(:exists?).and_return false }
+      it { is_expected.to be false }
     end
   end
 end
