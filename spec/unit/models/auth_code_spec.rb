9b0dd6792d4b3122b6ef3191ab662eb03795584f
clarkenciel <clarkenciel@gmail.com>
2018-04-15 10:26:48 -0700

    ✅ (auth-code): fixing failing test

--- spec/unit/models/auth_code_spec.rb
+++ spec/unit/models/auth_code_spec.rb
@@ -4,8 +4,8 @@ require 'rails_helper'
 
 RSpec.describe AuthCode, type: :model do
   describe 'attributes' do
-    subject { described_class.attributes }
+    subject { described_class.new }
 
-    it { is_expected.to include :code }
+    it { is_expected.to have_attribute :code }
   end
 end
