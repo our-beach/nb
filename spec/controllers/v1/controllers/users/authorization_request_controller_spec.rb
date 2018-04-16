7782c27a73b4b36b59c434cb8a302550a973d4f9
clarkenciel <clarkenciel@gmail.com>
2018-04-15 19:52:37 -0700

    🚚 (specs): get rid of "unit" subdir

--- spec/controllers/v1/controllers/users/authorization_request_controller_spec.rb
+++ spec/controllers/v1/controllers/users/authorization_request_controller_spec.rb
@@ -1,11 +1,9 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 
-RSpec.describe V1::SignUpsController, type: :controller do
+RSpec.describe Users::AuthorizationRequestController, type: :controller do
   describe 'create' do
     subject { response }
-    before { post :create, params: attributes }
+    before { post :create, params: api_params(data: attributes) }
 
     context 'When user sends valid phone number' do
       let(:attributes) { { phone_number: '15127628941' } }
@@ -19,7 +17,15 @@ RSpec.describe V1::SignUpsController, type: :controller do
       it { is_expected.to have_http_status 422 }
 
       it 'should include a message about the phone number being invalid' do
-        expect(JSON.parse(response.body)['message']).to eq("Invalid phone number")
+        response = JSON.parse(response.body)
+        expect(response).to have_attributes(
+          'errors' => include(
+            include(
+              'title' => 'Invalid phone number',
+              'description' => '"qwertyuiop" is not a valid phone number.'
+            ),
+          )
+        )
       end
     end
   end
