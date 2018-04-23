31927239335daff906a78e582196eefdca47f2b9
fakemonster <jgraythel@gmail.com>
2018-04-22 22:53:17 -0700

    🐛 (kek-spec): Remove override of environment variable
    Seems like `ENV['KEK'] = 'abc'` overwrites the actual env variable for
    the duration of the test.

--- spec/services/kek_service_spec.rb
+++ spec/services/kek_service_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe KekService do
   describe '.call' do
     subject { described_class.call }
-    before { ENV['KEK'] = 'abc' }
-    it { is_expected.to be == 'abc' }
+    it { is_expected.to be == ENV['KEK'] }
   end
 end
