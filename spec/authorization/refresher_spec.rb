103c1e92506e591109ed94dcbf051f63fdc095ee
clarkenciel <clarkenciel@gmail.com>
2018-12-02 22:01:23 -0800

    ✅ flesh out refresher spec
    @fakemonster this is another ping candidate!

--- spec/authorization/refresher_spec.rb
+++ spec/authorization/refresher_spec.rb
@@ -3,23 +3,25 @@ require 'rails_helper'
 RSpec.describe Authorization::Refresher do
   let(:service) { described_class.new db: db }
   let(:db) { double }
+  let(:tok) { instance_double AuthorizationToken }
 
   describe '#refresh!' do
-    context 'when no token exists that matches the provided token' do
-      xit 'should raise an Authorization::RefreshError' do
-
+    context 'when then token cannot be refreshed' do
+      before do
+        # what exactly does it mean for a token to be unrefreshable?
+        # does this case make sense?
       end
-    end
 
-    context 'when a matching token exists, but it cannot be refreshed' do
-      xit 'should raise an Authorization::RefreshError' do
-
+      it 'should raise an Authorization::RefreshError' do
+        expect { service.refresh! tok }.
+          to raise_error Authorization::RefreshError
       end
     end
 
     context 'when a token exists that can be refreshed' do
-      xit 'should return the refreshed token' do
-
+      it 'should return the refreshed token' do
+        refreshed = service.refresh! tok
+        expect(refreshed.expiration_time).to be > tok.expiration_time
       end
     end
   end
